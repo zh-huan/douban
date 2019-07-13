5e38cfc99c47af67e5ad9c805438bf8bda8dbb52
zh-huan <zhanghuan790@gmail.com>
2019-07-13 17:59:46 +0800

    提交部分样式

---
+++ web/componet/contentComponet.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="section" :class="type">
+        <div class="side"></div>
+        <div class="main"></div>
+    </div>
+</template>
+<script>
+    export default {
+        props:{
+            type:String,
+            contents:Object
+        },
+        data(){
+            return{
+
+            }
+        },
+        mounted(){
+            console.log(this.contents);
+        }
+    }
+</script>
